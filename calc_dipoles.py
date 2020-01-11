99f17f1b4dee3946ff91deffd76420476da4de84
Spencer Guo <spencerchenguo@gmail.com>
2020-01-11 13:24:02 -0800

    updates to calc-dipoles and removed copies of folder

--- calc_dipoles.py
+++ calc_dipoles.py
@@ -7,6 +7,12 @@ import pickle
 def load_trajectory():
     xyz_file = sys.argv[1]
     pdb_file = sys.argv[2]
+    print('XYZ file:' + xyz_file)
+    print('PDB file:' + pdb_file)
+    if not os.path.exists(xyz_file):
+        print('{0} does not exist'.format(xyz_file))
+    if not os.path.exists(pdb_file):
+        print('{0} does not exist'.format(pdb_file))
     print('Loading trajectory from {0}.....'.format(xyz_file))
     traj = md.load_xyz(xyz_file, pdb_file)
     # traj = md.load_xyz(xyz_file, pdb_file, frame=0)
@@ -26,8 +32,8 @@ def load_charges():
     for i in range(nframes):
         full_name = file_header + str(i) + file_name
         # check for file path
-        if not os.path.exists(input_path):
-            print("%s doesn't exist!" %input_path)
+        if not os.path.exists(full_name):
+            print("%s doesn't exist!" %full_name)
             sys.exit(1)
         if i % 1000 == 0:
             print('Loaded charges from frame {0}'.format(i))
@@ -43,6 +49,7 @@ def load_charges():
 
 
 def main():
+    print('Starting calculation....')
     traj = load_trajectory()
     charges = load_charges()
     print('Calculating dipole moments....')
