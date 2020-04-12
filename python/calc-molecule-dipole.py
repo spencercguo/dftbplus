0e54ee02fd7594c361cbf1f3094085746c8f2505
Spencer Guo <spencerchenguo@gmail.com>
2020-04-12 13:05:01 -0500

    molecular dipole script

--- python/calc-molecule-dipole.py
+++ python/calc-molecule-dipole.py
@@ -4,72 +4,73 @@ import os
 import ir
 
 def load_charges(data_dir, charge_filename):
-    charges = np.zeros((2, nframes), dtype=np.float64)
-    # get charges for two H atoms
-    natoms = 384
+    # load charges only for Hs
+    nmols = 128
+
+    # 128 x 2 x 10000
+    # 128 molecules, 2 charge, 10000 frames
+    charges = np.zeros((nmols, 2, nframes), dtype=np.float64)
+
     for i in range(nframes):
         charge_dir = 'data-{0}'.format(i)
         charge_file = os.path.join(data_dir, charge_dir, charge_filename)
         if not os.path.exists(charge_file):
             print('{0} does not exist!'.format(traj_file))
-        
+
         with open(charge_file, mode='r') as f:
-            
-            start_index = 14 + index * i
-            charges[0, i] = float(
+            for atom, line in enumerate(f):
+                if 14 <= atom <= 397:
+                    mol_index = atom // 3
+                    # first H atom
+                    if atom % 3 == 0:
+                        charges[mol_index, 0, i] = float(line.split()[1])
+                    # second H atom
+                    elif atom % 3 == 1:
+                        charges[mol_index, 1, i] = float(line.split()[1])
 
-def get_atom_coords(nframes):
+    return charges
+
+def calc_oh_vectors(traj_file):
     """
-    Get atomic coordinates for atoms at a single frame
+    Get O-H vectors for each frame
     """
-    # num atoms x xyz x nframes
-    xyz = np.zeros((384, 3, nframes), dtype=np.float64)
-    for i in range(nframes):
-        o_index = 386 * i + 3 + index * 3
-        o_line = lines[o_index]
-        h1_line = lines[o_index + 1]
-        h2_line = lines[o_index + 2]
+    # num atoms x xyz
+    xyz = np.zeros((384, nframe, 3), dtype=np.float64)
 
-        o_xyz = float(o_line.split()[1:])
-        h1_xyz = float(h1_line.split()[1:])
-        h2_xyz = float(h2_line.split()[1:])
+    # shape 128 x 2 x 3 x 100000
+    # 2 vectors, each xyz, for each frame and for each water molecule
+    oh_vectors = np.zeros((128, 2, 3, nframes), dtype=np.float64)
 
-
-def calc_oh_vectors():
-    # shape 2 x 3 x 100000
-    # 2 vectors, each xyz, for each frame
-    oh_vectors = np.zeros((2, 3, nframes), dtype=np.float64)
-    for i in range(nframes):
-        # calculate position vector for O-H bonds
-        #
-        # 384
-        #  i = step, time = ...
-        #  O    12....
-        #  H    2.33...
-        #  H    12.65...
-        o_index = 386 * i + 3 + index * 3
-        o_line = lines[o_index]
-        h1_line = lines[o_index + 1]
-        h2_line = lines[o_index + 2]
-
-        o_xyz = float(o_line.split()[1:])
-        h1_xyz = float(h1_line.split()[1:])
-        h2_xyz = float(h2_line.split()[1:])
-        oh_vector_1 = o_xyz - h1_xyz
-        oh_vector_2 = o_xyz - h2_xyz
-
-def calc_dipoles():
-    dipoles = np.zeros((3, nframes), dtype=np.float64)
     with open(traj_file, mode='r') as f:
-        lines = f.readlines()
+        for i in range(nframes):
+            for index in range(128):
+                # determine O-H vectors for each molecule
+                o_index = 386 * i + 3 + index * 3
+                o_line = lines[o_index]
+                h1_line = lines[o_index + 1]
+                h2_line = lines[o_index + 2]
 
-    # num atoms x xyz x nframes
-    xyz = np.zeros((384, 3, nframes), dtype=np.float64)
-    for i in range(nframes):
-        xyz[:,:,i] = get
-     
+                o_xyz = float(o_line.split()[1:])
+                h1_xyz = float(h1_line.split()[1:])
+                h2_xyz = float(h2_line.split()[1:])
+                
+                oh_vector_1 = o_xyz - h1_xyz
+                oh_vector_2 = o_xyz - h2_xyz
 
-        
+                oh_vectors[index, :, :, i] = oh_vector_1, oh_vector_2
+
+    return oh_vectors
+
+def calc_dipoles(charges, vectors):
+    # charges: 128 x 2 x 10000
+    # vectors: 128 x 2 x 3 x 10000
+    # dipoles: 128 x 10000 x 3
+    dipoles = charges[:, 0, :] * vectors[:, 0, :, :] + charges[:, 1, :] * vectors[:, 1, :, :]
+    print(dipoles.shape)
+
+    return dipoles
+
+
 def main():
     nframes = 100000
 
@@ -78,12 +79,25 @@ def main():
     data_dir = os.path.join(scratch, 'data/')
     charge_filename = 'detailed.out'
 
-# input trajectory
-traj_file = sys.argv[0]
-if not os.path.exists(traj_file):
-    print('{0} does not exist!'.format(traj_file))
+    # input trajectory
+    traj_file = sys.argv[0]
+    if not os.path.exists(traj_file):
+        print('{0} does not exist!'.format(traj_file))
 
-# which water molecule (0 indexed)
-index = sys.argv[1]
+    # which water molecule (0 indexed)
+    # index = sys.argv[1]
+    
+    # Calculate molecular dipoles for each of 
+    # extract charges
+    charges = load_charges(data_dir, charge_filename)
+    print(charges[:10,:,0])
+    
+    # num atoms x xyz x nframes
+    vectors = calc_oh_vectors(traj_file)
+    print(vectors[0, :, :, 0])
 
- 
+    # calculate dipoles
+    dipoles = calc_dipoles(charges, vectors)
+
+if __name__ == "__main__":
+    main()
